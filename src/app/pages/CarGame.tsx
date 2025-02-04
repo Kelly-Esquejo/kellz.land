d8ed299d8996bfab09d7ee2e9d65962771cf4be3
esquek1 <esquejokelly@gmail.com>
2025-02-03 17:46:58 -0800

    Initial commit

---
+++ src/app/pages/CarGame.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CarGame() {
+    return <div>CarGame</div>;
+}
+
+export default CarGame;
---
+++ src/app/pages/MemoryCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// Pixel art from https://pixelfrog-assets.itch.io/pixel-adventure-1
+//icons8.com/icons/color-pixels
+function MemoryCard() {
+    return <div>MemoryCard</div>;
+}
+
+export default MemoryCard;
