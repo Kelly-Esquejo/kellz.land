1e92b9add7c763edf167b62cf8b5b5f234b593e5
esquek1 <esquejokelly@gmail.com>
2025-02-04 22:26:34 -0800

    Tesseract does not work well

--- src/app/guessthecar/page.tsx
+++ src/app/guessthecar/page.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-
+import Tesseract from "tesseract.js";
 /* 
     From 59 car brands
     Pull images from Google Drive
@@ -11,10 +12,56 @@ import Image from "next/image";
                     Year
 
 */
-const GuessTheCar = () => {
+// Replace with Google Drive direct links if needed
+
+const carImages = [
+    "/toyota-86-2016.png",
+    "/shelby427Cobra-1966.png",
+    "/HONDA-CIVIC-TYPE-R-CAR.png",
+];
+
+const GuessTheCar: React.FC = () => {
+    const [carImage, setCarImage] = useState<string>(carImages[0]);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    const getRandomCar = () => {
+        setCarImage(carImages[Math.floor(Math.random() * carImages.length)]);
+    };
+
+    // Function to process the image and use Tesseract.js to detect text
+    const processImageForText = (imageSrc: string) => {
+        Tesseract.recognize(imageSrc, "eng", {
+            logger: (m) => console.log(m), // Optional: logs progress info
+        })
+            .then(({ data: { text } }) => {
+                console.log("Detected Text:", text); // Logs detected text
+            })
+            .catch((error) => {
+                console.error("Error with Tesseract.js:", error);
+            });
+    };
+
+    useEffect(() => {
+        // Process the current car image when it's set
+        processImageForText(carImage);
+    }, [carImage]);
+
     return (
-        <div>
-            <Image className="" src="/toyota-86-2016.png" alt="" fill />
+        <div className="flex flex-col items-center justify-center h-screen w-full gap-4">
+            {/* Processed Image with Hidden Logos */}
+
+            <Image
+                className="max-w-full h-auto"
+                src={carImage}
+                alt="Car"
+                width={1000}
+                height={600}
+            />
+            <button
+                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                onClick={getRandomCar}>
+                New Car
+            </button>
         </div>
     );
 };
---
+++ src/app/guessthelogo/GuessLogo.tsx
@@ -0,0 +1,9 @@
+// Pixel art from https://pixelfrog-assets.itch.io/pixel-adventure-1
+//icons8.com/icons/color-pixels
+import React from "react";
+
+function GuessLogo() {
+    return <div>Guess the Logo</div>;
+}
+
+export default GuessLogo;
