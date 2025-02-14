2dbbe77994600fc17ad165e49a62d95eda1be72f
esquek1 <esquejokelly@gmail.com>
2025-02-14 12:28:00 -0800

    Change font to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ const logos = [
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-mono)]">
             <header>
                 <h1 className="">kellz.land</h1>
             </header>
