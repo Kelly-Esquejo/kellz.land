32cd33a7a242f7c9789ca7889f96fc7ce2139411
esquek1 <esquejokelly@gmail.com>
2025-02-16 00:10:13 -0800

    Fix font path

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     // Comment the followig 1 line of code to run website locally
     basePath: "/kellz.land",
-    output: "export",
-    assetPrefix: "/kellz.land/",
+    // output: "export",
+    // assetPrefix: "/kellz.land/",
 };
 
 export default nextConfig;
