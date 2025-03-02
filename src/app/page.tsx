43580098a47f709c165797e6f18b6712ad2246ce
esquek1 <esquejokelly@gmail.com>
2025-03-01 20:27:48 -0800

    Changed link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,22 @@ const logos = [
         className: "coming-soon",
     },
 ];
+let vehicle = {
+    wheels: "4",
+    fuelType: "Gasoline",
+    color: "Green",
+};
+let carProps = {
+    type: {
+        value: "Volkswagen",
+    },
+    model: {
+        value: "Golf",
+    },
+};
 
+let car = Object.create(vehicle, carProps);
+console.log(car);
 export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-mono)]">
@@ -64,7 +79,8 @@ export default function Home() {
                     <li className="mb-2 ">
                         <a
                             className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold"
-                            href="https://neal.fun/">
+                            href="https://neal.fun/"
+                        >
                             https://neal.fun/
                         </a>
                     </li>
@@ -77,7 +93,8 @@ export default function Home() {
                         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
                         href="https://www.linkedin.com/in/kelly-esquejo/"
                         target="_blank"
-                        rel="noopener noreferrer">
+                        rel="noopener noreferrer"
+                    >
                         <Image
                             unoptimized
                             src="window.svg"
@@ -89,9 +106,10 @@ export default function Home() {
                     </a>
                     <a
                         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                        href="https://github.com/Kelly-Esquejo/kellz.fun"
+                        href="https://kellz-land.netlify.app/"
                         target="_blank"
-                        rel="noopener noreferrer">
+                        rel="noopener noreferrer"
+                    >
                         <Image
                             unoptimized
                             src="globe.svg"
