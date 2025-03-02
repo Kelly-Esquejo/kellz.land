61126bb8fd51e321355ff28b162f953f7abc5b7d
esquek1 <esquejokelly@gmail.com>
2025-03-01 20:52:56 -0800

    Test: make font relative path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,22 +44,7 @@ const logos = [
         className: "coming-soon",
     },
 ];
-let vehicle = {
-    wheels: "4",
-    fuelType: "Gasoline",
-    color: "Green",
-};
-let carProps = {
-    type: {
-        value: "Volkswagen",
-    },
-    model: {
-        value: "Golf",
-    },
-};
 
-let car = Object.create(vehicle, carProps);
-console.log(car);
 export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-mono)]">
